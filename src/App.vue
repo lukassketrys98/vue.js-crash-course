0f79ac248e3df78196b33917efd1cd33d0d19feb
Lukas Sketrys <lukas.sketrys98@gmail.com>
2020-05-12 15:20:46 +0300

    created

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-   
+    <FormHelper/>
   </div>
 </template>
 
 <script>
+
+import formHelper from './components/formHelper.vue'
 export default {
+  components:{
+    'FormHelper': formHelper
+  },
   name: 'app',
   data () {
     return {
---
+++ src/components/formHelper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <p>from </p>
+  </div>
+   
+
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
